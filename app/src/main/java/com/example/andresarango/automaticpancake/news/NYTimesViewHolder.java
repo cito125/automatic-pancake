84c6e80eaff62b043f9e8d65e87e263e6f10da65
cito125 <cito125@gmail.com>
2016-11-06 15:08:20 -0500

    this is an issue

--- app/src/main/java/com/example/andresarango/automaticpancake/news/NYTimesViewHolder.java
+++ app/src/main/java/com/example/andresarango/automaticpancake/news/NYTimesViewHolder.java
@@ -17,6 +17,7 @@ import java.util.List;
  */
 
 public class NYTimesViewHolder extends CardViewHolder {
+    int yo =0;
     Context mContext;
     private RecyclerView mNewsOptionRecycler;
     private List<String> mNewsOptionList = new ArrayList<String>(Arrays.asList("home",
